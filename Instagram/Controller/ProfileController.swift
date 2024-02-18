092bd376cd8a93be415f2205d8c97cec5d83c81e
DanHozan123 <danhozan@gmail.com>
2024-02-18 22:51:30 +0200

    setting up tab bar controller

--- Instagram/Controller/ProfileController.swift
+++ Instagram/Controller/ProfileController.swift
@@ -11,7 +11,7 @@ class ProfileController: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemOrange
+        view.backgroundColor =  .systemOrange
     }
     
 }
